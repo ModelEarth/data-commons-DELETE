82e7869593d456af80ed31fbf36102297d62e0c1
PJ <pchj@nmrfam.wisc.edu>
2024-03-30 01:10:40 -0500

    Delete docs/air/scripts/dcid/js directory

--- docs/air/scripts/dcid/js/dataFetcher.js
+++
@@ -1,7 +0,0 @@
-// dataFetcher.js
-export async function fetchJsonData(url) {
-  const response = await fetch(url);
-  if (!response.ok) throw new Error('Network response was not ok');
-  return response.json();
-}
-
