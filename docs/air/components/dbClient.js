5b0c5d5d9e3fd532ea60024703800cbca1fecf66
PJ <pchj@nmrfam.wisc.edu>
2024-03-30 02:06:43 -0500

    Update dbClient.js

--- docs/air/components/dbClient.js
+++ docs/air/components/dbClient.js
@@ -1,18 +1,25 @@
 import { fetchJsonData } from './dataFetcher.js';
 import * as duckdb from '@duckdb/duckdb-wasm';
+import { DuckDB, DuckDBBindings } from "@duckdb/duckdb-wasm/dist/duckdb-esm.js";
 
-export class DatabaseClient {
-  constructor() {
-    this.db = new duckdb.DuckDB(); // Corrected this line
-  }
+async function initDuckDB() {
+    const duckdb_wasm = await DuckDBBindings();
+    const db = new DuckDB(duckdb_wasm);
+    return db;
+}
 
-  async loadDataFromUrl(url) {
+// Call initDuckDB inside an async function or use await directly if in an async context
+(async () => {
+    this.db = await initDuckDB();
+})();
+
+async function loadDataFromUrl(url) {
     const jsonData = await fetchJsonData(url);
     await this.createTable();
     await this.insertData(jsonData);
-  }
+}
 
-  async createTable() {
+async function createTable() {
     const connection = await this.db.connect();
     // Create table if not exists
     await connection.query(`
@@ -23,31 +30,31 @@ export class DatabaseClient {
       )
     `);
     connection.close();
-  }
+}
 
-  async insertData(jsonData) {
+async function insertData(jsonData) {
     const connection = await this.db.connect();
     // Insert data into the table
     for (const countryData of jsonData) {
-      const countryCode = countryData.country_code;
-      for (const data of countryData.data) {
-        const year = data.year;
-        const emission = data.emission;
-        await connection.query(`
+        const countryCode = countryData.country_code;
+        for (const data of countryData.data) {
+            const year = data.year;
+            const emission = data.emission;
+            await connection.query(`
           INSERT INTO emissions (country_code, year, emission)
           VALUES ('${countryCode}', '${year}', ${emission})
         `);
-      }
+        }
     }
     connection.close();
-  }
+}
 
-  async queryData() {
+async function queryData() {
     const connection = await this.db.connect();
     // Placeholder for querying DuckDB
     // Return the query result directly
     const result = await connection.query('SELECT * FROM emissions');
     connection.close();
     return result;
-  }
 }
+
