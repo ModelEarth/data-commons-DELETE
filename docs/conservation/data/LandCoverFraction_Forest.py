0e5a4a0720423a210969a565e470e54855e49bea
modelearth <model@dreamstudio.com>
2024-04-09 22:27:10 -0400

    LandCover and weather data

--- docs/conservation/data/LandCoverFraction_Forest.py
+++ docs/conservation/data/LandCoverFraction_Forest.py
@@ -1,4 +1,12 @@
-import datacommons as dc
+
+# python3 -m venv env && source env/bin/activate
+
+# Install datacommons
+# !pip install datacommons_pandas --upgrade --quiet
+
+import datacommons_pandas as dc
+import pandas as pd
+import os
 
 # Retrieve all counties in Georgia (state code: 13)
 georgia_counties_info = dc.get_places_in(['geoId/13'], 'County')
@@ -11,7 +19,12 @@ forest_cover_data = {}
 for county_dcid in georgia_counties_dcids:
     forest_cover_data[county_dcid] = dc.get_stat_all([county_dcid], ["LandCoverFraction_Forest"])
 
-print(forest_cover_data)
+#print(forest_cover_data)
 
+df = pd.DataFrame(forest_cover_data)
+outputFolder = ""
+output_name = "LandCoverFraction_Forest.csv"
+df.to_csv(os.path.join(outputFolder, output_name),index=False)
+            
 # pulls all available years but its out of order and will need to sort it with a JSON parser in descending/ascending order:
 # 'LandCoverFraction_Forest': {'sourceSeries': [{'val': {'2015': 57.712213157748906, '2019': 56.10225251926496, '2017': 57.329201724571796, '2016': 57.08418588188137, '2018': 58.540199494228055}
