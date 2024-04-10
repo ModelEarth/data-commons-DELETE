4c5d0c6e244f595c30a52533f19492e2cf9b7e4a
modelearth <model@dreamstudio.com>
2024-04-09 22:53:04 -0400

    py

--- docs/conservation/data/LandCoverFraction_Forest.py
+++ docs/conservation/data/LandCoverFraction_Forest.py
@@ -1,6 +1,4 @@
 
-# python3 -m venv env && source env/bin/activate
-
 # Install datacommons
 # !pip install datacommons_pandas --upgrade --quiet
 
@@ -19,12 +17,11 @@ forest_cover_data = {}
 for county_dcid in georgia_counties_dcids:
     forest_cover_data[county_dcid] = dc.get_stat_all([county_dcid], ["LandCoverFraction_Forest"])
 
-#print(forest_cover_data)
-
 df = pd.DataFrame(forest_cover_data)
 outputFolder = ""
 output_name = "LandCoverFraction_Forest.csv"
 df.to_csv(os.path.join(outputFolder, output_name),index=False)
 print("Saved: " + output_name)
+
 # pulls all available years but its out of order and will need to sort it with a JSON parser in descending/ascending order:
 # 'LandCoverFraction_Forest': {'sourceSeries': [{'val': {'2015': 57.712213157748906, '2019': 56.10225251926496, '2017': 57.329201724571796, '2016': 57.08418588188137, '2018': 58.540199494228055}
